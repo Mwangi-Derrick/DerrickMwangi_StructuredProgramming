b9aacaee0b7cb444baf3e764fe3be78a6899afb3
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-01-14 22:44:38 +0300

    feat: Sort Array Ascending with bubble asor algorithim

---
+++ Assignment4/task5/task5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+
+  // Prompt user for the size of array
+  printf("Input the size of array : ");
+  scanf("%d", &n);
+
+  // Declare array of size n
+  int arr[n];
+
+  // Read elements into the array
+  printf("Input %d elements in the array :\n", n);
+  for (int i = 0; i < n; i++) {
+    printf("element - %d : ", i);
+    scanf("%d", &arr[i]);
+  }
+
+  // Bubble sort algorithm to sort in ascending order
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap arr[j] and arr[j+1]
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  // Display sorted array
+  printf("\nElements of array in sorted ascending order: ");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
